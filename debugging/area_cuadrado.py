c5ef76bb6490e6bab76cc3f94f3410dd973c3d92
Matias Garnica <garnicamatias@outlook.es>
2023-06-29 16:18:25 -0300

    Debugging

---
+++ debugging/area_cuadrado.py
@@ -0,0 +1,11 @@
+def calcular_area_cuadrado(lado):
+    area = lado*lado
+    return area
+
+lado_cuadrados = [1, 3, 4]
+area_cuadrados = []
+for lado in lado_cuadrados:
+    area = calcular_area_cuadrado(lado)
+    area_cuadrados.append(area)
+
+#python -m pdb area_cuadrado.py
